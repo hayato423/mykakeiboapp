c76cd06c5c4004686dc1831b148cc28809bfed43
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-17 04:14:14 +0900

    add insertion userInfo

--- routes/register.js
+++ routes/register.js
@@ -1,19 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const pg = require('pg');
+const bcrypt = require("bcryptjs");
+const { Client } = require("pg");
 
-const connectinoString = 'postgres://postgres:postgres@localhost:5432/mykakeiboapp';
-
-router.get('/',function(req,res,next){
-    res.render('register',{title:'アカウント登録'});
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/mykakeiboapp';
+const client = new Client({
+  connectionString: connectionString
 });
 
-router.post('/',function(req,res,next){
-    const saltRounds = 10; //ストレッチング回数
-    const username = req.body['username'];
-    const plaintextPassword = req.body['password'];
-    var hash = bcrypt.hashSync(plaintextPassword,saltRounds);
-})
+router.get("/", function (req, res, next) {
+  res.render("register", { title: "アカウント登録" });
+});
+
+router.post("/", function (req, res, next) {
+  const saltRounds = 4; //ストレッチング回数
+  const username = req.body["username"];
+  const plaintextPassword = req.body["password"];
+  var hash = bcrypt.hashSync(plaintextPassword, saltRounds);
+  client.connect();
+  sql = "INSERT INTO users (username,password) VALUES($1,$2)";
+  const values = [username, hash];
+  client
+    .query(sql, values)
+    .then((result) => {
+      console.log(result);
+      client.end();
+    })
+    .catch((e) => console.error(e.stack));
+  res.render("registered", { title: "登録しました" });
+});
 
 module.exports = router;
