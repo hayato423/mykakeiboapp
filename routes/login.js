e51cb671b6a3af9c0dec76a735832e7430f36ace
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-18 11:53:31 +0900

    add login failed process

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,8 @@ const client = new Client({
 });
 
 router.get("/", function (req, res, next) {
-  res.render("login", { title: "ログイン" });
+  var msg = "ユーザーネームとパスワードを入力してください";
+  res.render("login", { title: "ログイン", msg : msg });
 });
 
 router.post("/", function (req, res, next) {
@@ -26,6 +27,12 @@ router.post("/", function (req, res, next) {
       req.session.username = username;
       res.redirect('..');
     }
+    else{
+      res.render('login',{
+        title:'ログイン',
+        msg : 'ログインに失敗しました'
+      });
+    }
   });
 });
 
@@ -35,6 +42,7 @@ async function comfirmPassword(username, password) {
     client.connect();
     const sql = "SELECT password FROM users WHERE username = $1";
     const result = await client.query(sql, [username]);
+    client.end();
     let hash = result.rows[0]["password"];
     const isCorrectPassword = await bcrypt.compare(password,hash);
     if(isCorrectPassword) return true;
