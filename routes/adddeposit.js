553180bd0cbd313282f8c7d71662ac25dc3cea0f
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-20 10:47:16 +0900

    insert data into db

--- routes/adddeposit.js
+++ routes/adddeposit.js
@@ -12,7 +12,7 @@ const client = new Client({
 
 router.get("/", function (req, res) {
   //ログインされていなかったらindexにリダイレクト
-  if (req.session.isLogin == false) {
+  if (req.session.isLogin == false || req.session.isLogin == undefined) {
     res.redirect("..");
   } else
     res.render("addDeposit", {
@@ -20,5 +20,22 @@ router.get("/", function (req, res) {
     });
 });
 
+router.post('/',function(req,res) {
+    const username = req.session.username;
+    const date = req.body['date'];
+    const content = req.body['content'];
+    const amount = req.body['amount'];
+    console.log(date + ":" + username +" " + content +" "+ amount );
+    client.connect();
+    const sql = "INSERT INTO deposit VALUES( $1, $2, $3, $4);";
+    const values = [date,username,content,amount];
+    client.query(sql,values)
+    .then((result) => {
+        console.log(result);
+        client.end();
+    })
+    .catch((e) => console.log(e.stack));
+    res.redirect('..');
+})
 
 module.exports = router;
