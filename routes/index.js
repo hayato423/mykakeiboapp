829056ae8bf182fb7c8e2124cfc5b70395219cf1
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-22 04:52:32 +0900

    if possesion = null display 0

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const pool = require('../postgresPool');
+const pool = require("../postgresPool");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   if (req.session.isLogin != undefined && req.session.isLogin == true) {
     const username = req.session.username;
     displayPossesion(username).then((possesion) => {
+      if (possesion === null) possesion = 0;
       res.render("indexAfterLogin", {
         title: "マイ家計簿",
         username: username,
-        possesion: possesion
+        possesion: possesion,
       });
     });
   } else res.render("index", { title: "マイ家計簿" });
@@ -19,7 +20,7 @@ router.get("/", function (req, res, next) {
 async function displayPossesion(username) {
   const sql = "SELECT SUM(amount) from deposit WHERE username = $1";
   const result = await pool.query(sql, [username]);
-  const possesion = result.rows[0]['sum'];
+  const possesion = result.rows[0]["sum"];
   console.log(possesion);
   return possesion;
 }
