6f00aa9c4bd5f2afdf8286dffe8b2e7ba671c59f
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-23 16:50:05 +0900

    display netPossesion

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,18 @@ var router = express.Router();
 const pool = require("../postgresPool");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/",async function (req, res, next) {
   if (req.session.isLogin != undefined && req.session.isLogin == true) {
     const username = req.session.username;
-    displayPossesion(username).then((possesion) => {
-      if (possesion === null) possesion = 0;
-      res.render("indexAfterLogin", {
-        title: "マイ家計簿",
-        username: username,
-        possesion: possesion,
-      });
+    let possesion = await displayPossesion(username);
+    let netPossesion = await displayNetPossesion(possesion,username);
+    if(possesion == null) possesion = 0;
+    if(netPossesion == null) netPossesion = 0;
+    res.render("indexAfterLogin", {
+      title: "マイ家計簿",
+      username: username,
+      possesion: possesion,
+      netPossesion : netPossesion
     });
   } else res.render("index", { title: "マイ家計簿" });
 });
@@ -28,6 +30,14 @@ async function displayPossesion(username) {
   console.log("paied:"+paied);
   const possesion = deposit - paied;
   return possesion;
+}
+
+async function displayNetPossesion(possesion,username) {
+  const sql = "SELECT SUM(price) from card WHERE username = $1";
+  let result = await pool.query(sql,[username]);
+  const sum_price = result.rows[0]['sum'];
+  const netPossesion = possesion - sum_price;
+  return netPossesion;
 }
 
 module.exports = router;
