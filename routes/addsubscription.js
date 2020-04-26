cca7fa527567bafabc3c3c2a1b5efad4ff3d237a
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-04-26 15:35:17 +0900

    add post process

--- routes/addsubscription.js
+++ routes/addsubscription.js
@@ -12,4 +12,24 @@ router.get('/', function(req,res) {
     }
 });
 
+
+router.post('/', async function(req,res) {
+    const username = req.session.username;
+    const payment_date = req.body['payment_date'];
+    const product_name = req.body['product_name'];
+    const price = req.body['price'];
+    const method = req.body['method'];
+    console.log(payment_date + ',' + product_name + ',' + price + ',' + method);
+    try{
+        const sql = 'INSERT INTO subscription VALUES($1,$2,$3,$4,$5)';
+        const params = [payment_date,username,product_name,price,method];
+        const result = await pool.query(sql,params);
+        console.log(result);
+    }catch(e) {
+        console.error(e);
+    }finally{
+        res.redirect('..');
+    }
+});
+
 module.exports = router;
